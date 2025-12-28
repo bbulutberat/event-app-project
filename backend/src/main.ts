07e619c71b66c6130b8c9cb92218e3cd506c45d6
bbulutberat <bbulutberat@gmail.com>
2025-12-28 19:17:45 +0300

    güncelleme

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  // React'in bağlanmasına izin ver (CORS)
+  // CORS ayarını da ekleyelim ki Frontend (React) bağlanırken hata almasın
   app.enableCors(); 
+
+  // ESKİSİ MUHTEMELEN ŞÖYLEYDİ: await app.listen(3000);
   
-  await app.listen(3000);
+  // YENİSİ BU OLACAK 👇
+  // Render bir PORT verirse onu kullan, vermezse (bilgisayarındaysan) 3000'i kullan.
+  // '0.0.0.0' kısmı çok önemli, dış dünyaya açılmasını sağlar.
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
